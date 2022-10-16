bc9fef70ddba4037aa62e55f905189c9afac285c
Shabbir <sessaji@mediaocean.com>
2022-10-16 21:23:53 +0530

    Add toast

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useLoaderData } from 'react-router-dom';
 import Select from 'react-select';
+import { ToastContainer, toast, Slide } from 'react-toastify';
 import service from './service';
 import '../css/homepage.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const HomePage = () => {
   const [hofIdObj, setHOFIdObj] = useState({
@@ -40,6 +42,16 @@ const HomePage = () => {
               miqaatId: selectedMiqaat.miqaatId
             }
           });
+        } else {
+          toast.error('Incorrect HOF ITS ID', {
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+            transition: Slide,
+            theme: 'light'
+          });
         }
       });
   };
@@ -52,47 +64,50 @@ const HomePage = () => {
   };
 
   return (
-    <div className="homepage">
-      <h1>{selectedMiqaat ? selectedMiqaat.name : 'Select a miqaat'}</h1>
-      <div className="user-box">
-        {selectedMiqaat ? (
-          <>
-            <input
-              type="number"
-              placeholder="Enter HOF ITS ID"
-              required={true}
-              onChange={onHOFIdChange}
-              onInvalid={(event) =>
-                event.target.setCustomValidity('HOF ITS ID is required')
-              }
-            />
-            {hofIdObj?.isValid ? (
-              <button
-                type="submit"
-                className={
-                  !hofIdObj.isValid
-                    ? 'button button-enable'
-                    : 'button button-disable'
+    <>
+      <div className="homepage">
+        <h1>{selectedMiqaat ? selectedMiqaat.name : 'Select a miqaat'}</h1>
+        <div className="user-box">
+          {selectedMiqaat ? (
+            <>
+              <input
+                type="number"
+                placeholder="Enter HOF ITS ID"
+                required={true}
+                onChange={onHOFIdChange}
+                onInvalid={(event) =>
+                  event.target.setCustomValidity('HOF ITS ID is required')
                 }
-                pattern="[0-9]{0,8}"
-                onClick={onSubmit}
-                disabled={!hofIdObj?.isValid}
-              >
-                Submit
-              </button>
-            ) : null}
-          </>
-        ) : (
-          <Select
-            options={miqaatOptions}
-            placeholder={''}
-            onChange={(value) => {
-              setSelectedMiqaat(value.miqaat);
-            }}
-          />
-        )}
+              />
+              {hofIdObj?.isValid ? (
+                <button
+                  type="submit"
+                  className={
+                    !hofIdObj.isValid
+                      ? 'button button-enable'
+                      : 'button button-disable'
+                  }
+                  pattern="[0-9]{0,8}"
+                  onClick={onSubmit}
+                  disabled={!hofIdObj?.isValid}
+                >
+                  Submit
+                </button>
+              ) : null}
+            </>
+          ) : (
+            <Select
+              options={miqaatOptions}
+              placeholder={''}
+              onChange={(value) => {
+                setSelectedMiqaat(value.miqaat);
+              }}
+            />
+          )}
+        </div>
       </div>
-    </div>
+      <ToastContainer />
+    </>
   );
 };
 
